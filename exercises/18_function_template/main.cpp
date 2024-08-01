fe24a79c3017688df202979ce419a1cfb4822ab5
Turingg <732153168@qq.com>
2024-08-01 16:46:09 +0800

    9-20

--- exercises/18_function_template/main.cpp
+++ exercises/18_function_template/main.cpp
@@ -1,11 +1,15 @@
 ﻿#include "../exercise.h"
-
+#include <cmath>
+#include <cassert>
 // READ: 函数模板 <https://zh.cppreference.com/w/cpp/language/function_template>
 // TODO: 将这个函数模板化
-int plus(int a, int b) {
+template<class T>
+T plus(T a, T b) {
     return a + b;
 }
-
+bool float_equal(double a, double b, double epsilon = 1e-6) {
+    return std::fabs(a - b) < epsilon;
+}
 int main(int argc, char **argv) {
     ASSERT(plus(1, 2) == 3, "Plus two int");
     ASSERT(plus(1u, 2u) == 3u, "Plus two unsigned int");
@@ -14,7 +18,7 @@ int main(int argc, char **argv) {
     ASSERT(plus(1.25f, 2.5f) == 3.75f, "Plus two float");
     ASSERT(plus(1.25, 2.5) == 3.75, "Plus two double");
     // TODO: 修改判断条件使测试通过
-    ASSERT(plus(0.1, 0.2) == 0.3, "How to make this pass?");
+    ASSERT(float_equal(plus(0.1, 0.2), 0.3), "Plus two double with precision");
 
     return 0;
 }
